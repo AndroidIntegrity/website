d4574fdcd1eb162433e70a63655dd38bf38dc87c
Lucy Kushinada <lucykushinada00@gmail.com>
2024-12-10 00:13:28 +0530

    Updated About view impl

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,30 @@ import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/ca
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 
+type headerData = {
+    title: string;
+    description: string;
+}
+
+const headerData: headerData[] = [
+    {
+        title: 'Our Team',
+        description: 'We are a small collective of people passionate about customizing our Android devices and using them to their full extent.'
+    },
+    {
+        title: 'Our Mission',
+        description: 'Our goal is to rework the Play Integrity system to be inclusive of rooted devices, while maintaining and supporting security.'
+    },
+    {
+        title: 'Our Vision',
+        description: 'We aim to be a trusted third party to vet custom ROMs, in order to assist Google in being inclusive, yet secure.'
+    },
+    {
+        title: 'Get Involved',
+        description: 'Join us in our mission to promote a fair and open Android operating system. Sign our petition and follow us on social media.'
+    }
+]
+
 export const metadata: Metadata = {
   title: 'About | Android Integrity Alliance'
 }
@@ -14,30 +38,14 @@ export default function About() {
                 <p className="text-lg md:text-lg lg:text-base text-center md:w-1/2">We are the Android Integrity Alliance, a group of passionate individuals aiming to promote a fair and open Android operating system. Our mission is to support all types of users and ensure that everyone can enjoy the full potential of their device.</p>
             </section>
             <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 pt-8 px-4 sm:px-8">
-            <Card>
-                <CardHeader>
-                    <CardTitle>Our Team</CardTitle>
-                    <CardDescription>We are a small collective of people passionate about customizing our Android devices and using them to their full extent.</CardDescription>
-                </CardHeader>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Our Mission</CardTitle>
-                    <CardDescription>Our goal is to rework the Play Integrity system to be inclusive of rooted devices, while maintaining and supporting security.</CardDescription>
-                </CardHeader>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Our Vision</CardTitle>
-                    <CardDescription>We aim to be a trusted third party to vet custom ROMs, in order to assist Google in being inclusive, yet secure.</CardDescription>
-                </CardHeader>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Get Involved</CardTitle>
-                    <CardDescription>Join us in our mission to promote a fair and open Android operating system. Sign our petition and follow us on social media.</CardDescription>
-                </CardHeader>
-            </Card>
+                {headerData.map((data) => (
+                    <Card key={data.title}>
+                    <CardHeader>
+                        <CardTitle>{data.title}</CardTitle>
+                        <CardDescription>{data.description}</CardDescription>
+                    </CardHeader>
+                </Card>
+                ))}
         </section>
         </main>
     )
